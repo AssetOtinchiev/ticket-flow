6a1fa3faf4d7ac58b8b22312c4b96f14d33af714
AssetKlips <asseto@klips.com>
2026-03-01 17:23:26 +0500

    [edit] remove summaries

--- src/backend/ManualWork/ManualWork/Program.cs
+++ src/backend/ManualWork/ManualWork/Program.cs
@@ -9,11 +9,6 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
 app.MapGrpcService<GreeterService>();
 
 app.Run();
